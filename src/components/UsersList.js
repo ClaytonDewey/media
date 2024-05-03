7c10a2896fc498170fb08547c424aa0e3233e10c
ClaytonDewey <clay@dryadmedia.com>
2024-05-03 12:18:17 -0500

    Create & import addUser

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchUsers } from '../store';
-import { Skeleton } from '.';
+import { fetchUsers, addUser } from '../store';
+import { Skeleton, Button } from '.';
 
 function UsersList() {
   const dispatch = useDispatch();
@@ -13,6 +13,10 @@ function UsersList() {
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
+
+  const handleAddUser = () => {
+    dispatch(addUser());
+  };
 
   if (isLoading) {
     return <Skeleton times={4} className='h-10 w-full' />;
@@ -32,7 +36,15 @@ function UsersList() {
     );
   });
 
-  return <div>{renderedUsers}</div>;
+  return (
+    <div>
+      <div className='flex flex-row justify-between m-3'>
+        <h1 className='m-2 text-xl'>Users</h1>
+        <Button onClick={handleAddUser}>+ Add User</Button>
+      </div>
+      {renderedUsers}
+    </div>
+  );
 }
 
 export default UsersList;
