c4623d6403e751f173557d82e41667bcb91a4724
ClaytonDewey <clay@dryadmedia.com>
2024-05-07 13:29:00 -0500

    Add some styles and fix Skeleton

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -11,7 +11,7 @@ const AlbumsList = ({ user }) => {
 
   let content;
   if (isLoading) {
-    content = <Skeleton times={3} />;
+    content = <Skeleton className='h-10 w-full' times={3} />;
   } else if (error) {
     content = <div>Error loading albums.</div>;
   } else {
@@ -27,9 +27,11 @@ const AlbumsList = ({ user }) => {
 
   return (
     <div>
-      <div>
-        Albums for {user.name}
-        <Button onClick={handleAddAlbum}>+ Add Album</Button>
+      <div className='m-2 flex flex-row items-center justify-between'>
+        <h3 className='text-lg font-bold'>Albums for {user.name}</h3>
+        <Button loading={results.isLoading} onClick={handleAddAlbum}>
+          + Add Album
+        </Button>
       </div>
       <div>{content}</div>
     </div>
