3bf2d3ae98ae08cefa7a07a5e9cc7e4fec8f667a
ClaytonDewey <clay@dryadmedia.com>
2024-05-07 15:38:15 -0500

    Display photo and add delete function

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,8 +1,42 @@
-import { useFetchPhotosQuery } from '../store/apis/photosApi';
+import {
+  useFetchPhotosQuery,
+  useAddPhotoMutation,
+} from '../store/apis/photosApi';
+import { Button, Skeleton, PhotosListItem } from '.';
 
 function PhotosList({ album }) {
-  useFetchPhotosQuery(album);
-  return <div>PhotosList</div>;
+  const { data, isFetching, error } = useFetchPhotosQuery(album);
+  const [addPhoto, addPhotoResults] = useAddPhotoMutation();
+
+  const handleOnAddPhoto = () => {
+    addPhoto(album);
+  };
+
+  let content;
+
+  if (isFetching) {
+    content = <Skeleton className='h-8 w-8' times={4} />;
+  } else if (error) {
+    content = <div>Error fetching photos</div>;
+  } else {
+    content = data.map((photo) => {
+      return <PhotosListItem key={photo.id} photo={photo} />;
+    });
+  }
+
+  return (
+    <div>
+      <div className='m-2 flex flex-row items-center justify-between'>
+        <h3 className='text-lg font-bold'>Photos in {album.title}</h3>
+        <Button loading={addPhotoResults.isLoading} onClick={handleOnAddPhoto}>
+          + Add Photo
+        </Button>
+      </div>
+      <div className='mx-8 flex flex-row flex-wrap justify-center'>
+        {content}
+      </div>
+    </div>
+  );
 }
 
 export default PhotosList;
